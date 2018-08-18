c494d82523e92b25b79c55bc108ee4f1191630d6
Mike Falanga <m.falanga@gmail.com>
2018-08-18 00:35:53 -0400

    Manage environment variables

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
 const Path = require('path');
 const Hapi = require('hapi');
 const Inert = require('inert');
+const Dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') {
+  Dotenv.load();
+}
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
